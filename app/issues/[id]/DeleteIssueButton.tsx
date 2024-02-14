0e623e5439a0e1fb78add80e564ced61eef3f0c9
a656q59 <dpnr915@gmail.com>
2024-02-14 18:49:01 +0530

    show a confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,7 +1,88 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+// import { AlertDialogRoot, Button, Flex } from ;
+import { Button, Flex } from "@radix-ui/themes";
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
+
+// import {
+//   AlertDialogTrigger,
+//   AlertDialogContent,
+//   AlertDialogTitle,
+//   AlertDialogDescription,
+//   AlertDialogCancel,
+//   AlertDialogAction,
+// } from "@radix-ui/react-alert-dialog";
 
 const DeleteIssueButton = ({ bugId }: { bugId: number }) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger asChild>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Portal>
+        <AlertDialog.Content>
+          <div>
+            <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+
+            <AlertDialog.Description>
+              Are you sure? This action cannot be undone.
+            </AlertDialog.Description>
+
+            <Flex mt="4" gap="3">
+              <AlertDialog.Cancel asChild>
+                <Button variant="soft" color="gray">
+                  Cancel
+                </Button>
+              </AlertDialog.Cancel>
+
+              <AlertDialog.Action asChild>
+                <Button variant="solid" color="red">
+                  Revoke access
+                </Button>
+              </AlertDialog.Action>
+            </Flex>
+          </div>
+        </AlertDialog.Content>
+      </AlertDialog.Portal>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
+
+// import React from "react";
+// import * as AlertDialog from "@radix-ui/react-alert-dialog";
+
+// const DeleteIssueButton = ({ bugId }: { bugId: number }) => (
+//   <AlertDialog.Root>
+//     <AlertDialog.Trigger asChild>
+//       <Button color="red">Delete Issue</Button>
+//     </AlertDialog.Trigger>
+//     <AlertDialog.Portal>
+//       <AlertDialog.Overlay className="AlertDialogOverlay" />
+
+//       <AlertDialog.Content className="AlertDialogContent">
+//         <AlertDialog.Title className="AlertDialogTitle">
+//           Are you absolutely sure?
+//         </AlertDialog.Title>
+//         <AlertDialog.Description className="AlertDialogDescription">
+//           This action cannot be undone. This will permanently delete your
+//           account and remove your data from our servers.
+//         </AlertDialog.Description>
+//         <div style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}>
+//           <AlertDialog.Cancel asChild>
+//             <Button variant="soft" color="gray">
+//               Cancel
+//             </Button>
+//           </AlertDialog.Cancel>
+//           <AlertDialog.Action asChild>
+//             <Button variant="solid" color="red">
+//               Revoke access
+//             </Button>
+//           </AlertDialog.Action>
+//         </div>
+//       </AlertDialog.Content>
+//     </AlertDialog.Portal>
+//   </AlertDialog.Root>
+// );
+
+// export default DeleteIssueButton;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "@radix-ui/themes/styles.css";
+
 import "./theme-config.css";
 import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Container, Theme } from "@radix-ui/themes";
