d7050f7bed89b5f3ce9013e38ee0a4c176735410
a656q59 <dpnr915@gmail.com>
2024-02-19 17:07:34 +0530

    enhance the API to assign issues

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { bugSchema } from "@/app/validationSchema";
+import { patchBugSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -8,29 +8,47 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return NextResponse.json({}, { status: 401 });
-  }
+  // const session = await getServerSession(authOptions);
+  // if (!session) {
+  //   return NextResponse.json({}, { status: 401 });
+  // }
   const body = await request.json();
-  const validation = bugSchema.safeParse(body);
+
+  const validation = patchBugSchema.safeParse(body);
+
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
-  const bug = await prisma.bug.findUnique({
-    where: { id: parseInt(params.id) },
-  });
-  if (!bug) {
-    return NextResponse.json({ error: "invalid issue" }, { status: 404 });
+
+  const { assignedToUserId, title, description } = body;
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "invalid user" }, { status: 400 });
+    }
+
+    const bug = await prisma.bug.findUnique({
+      where: { id: parseInt(params.id) },
+    });
+
+    if (!bug) {
+      return NextResponse.json({ error: "invalid issue" }, { status: 404 });
+    }
+
+    const updatedBug = await prisma.bug.update({
+      where: { id: bug.id },
+      data: {
+        title,
+        description,
+        assignedToUserId,
+      },
+    });
+
+    return NextResponse.json(updatedBug);
   }
-  const updatedBug = await prisma.bug.update({
-    where: { id: bug.id },
-    data: {
-      title: body.title,
-      description: body.description,
-    },
-  });
-  console.log(updatedBug);
-  return NextResponse.json(updatedBug);
 }
 
 export async function DELETE(
