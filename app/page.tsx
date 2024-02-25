c2e120940ab988ffe16cfaa01d7111a43653c75d
a656q59 <dpnr915@gmail.com>
2024-02-25 08:27:02 +0530

    build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import prisma from "@/prisma/client";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.bug.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.bug.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.bug.count({ where: { status: "CLOSED" } });
+  return <IssueSummary open={open} inProgress={inProgress} closed={5} />;
 }
