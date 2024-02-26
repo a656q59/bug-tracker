f6af637116a798d16086c9fca8643ae39e557ed9
a656q59 <dpnr915@gmail.com>
2024-02-26 15:17:59 +0530

    Optimize performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,17 +7,20 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 interface Props {
   params: { id: string };
 }
+
+const fetchUser = cache((issueId: number) =>
+  prisma.bug.findUnique({ where: { id: issueId } })
+);
 
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
   if (typeof params.id !== "string") notFound();
 
-  const bug = await prisma.bug.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const bug = await fetchUser(parseInt(params.id));
   if (!bug) return notFound();
   return (
     <Grid columns={{ initial: "1", sm: "5" }} gap="5">
@@ -40,9 +43,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 export default IssueDetailPage;
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.bug.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchUser(parseInt(params.id));
 
   return {
     title: issue?.title,
