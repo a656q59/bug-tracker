be178c1267eda06635f9615acf3aaa72251e82ae
a656q59 <dpnr915@gmail.com>
2024-02-27 16:16:55 +0530

    created new migration

--- prisma/migrations/20240219091911_add_assigned_bugs/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `bug` ADD COLUMN `assignedToUserId` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Bug` ADD CONSTRAINT `Bug_assignedToUserId_fkey` FOREIGN KEY (`assignedToUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
