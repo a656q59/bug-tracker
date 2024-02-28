75d1dc65187e212bdda2e209658841ab6103f11f
a656q59 <dpnr915@gmail.com>
2024-02-28 14:00:17 +0530

    check 3

--- prisma/migrations/20240228082909_initial_migration/migration.sql
+++ prisma/migrations/20240228082909_initial_migration/migration.sql
@@ -1,4 +1,4 @@
-CreateTable
+-- CreateTable
 CREATE TABLE `Account` (
     `userId` VARCHAR(191) NOT NULL,
     `typeasd` VARCHAR(191) NOT NULL,
@@ -17,15 +17,15 @@ CREATE TABLE `Account` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CreateTable
+-- CreateTable
 CREATE TABLE `Bug` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
     `description` TEXT NOT NULL,
-    `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
+    `statuis` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `assignedToUserId` VARCHAR(255) NULL,
+    `assignedToUserId` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
