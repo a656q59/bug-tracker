83a15d3a5ddb55d9d0c2aae0f7fb80a871177a5f
a656q59 <dpnr915@gmail.com>
2024-02-28 11:57:08 +0530

    test2

--- prisma/migrations/20240228061645_initial_mgration/migration.sql
+++ prisma/migrations/20240228061645_initial_mgration/migration.sql
@@ -1,23 +1,23 @@
--- CreateTable
--- CREATE TABLE `Account` (
---     `userId` VARCHAR(191) NOT NULL,
---     `typeasd` VARCHAR(191) NOT NULL,
---     `provider` VARCHAR(191) NOT NULL,
---     `providerAccountId` VARCHAR(191) NOT NULL,
---     `refresh_token` TEXT NULL,
---     `access_token` TEXT NULL,
---     `expires_at` INTEGER NULL,
---     `token_type` VARCHAR(191) NULL,
---     `scope` VARCHAR(191) NULL,
---     `id_token` TEXT NULL,
---     `session_state` VARCHAR(191) NULL,
---     `id` VARCHAR(191) NOT NULL,
+CreateTable
+CREATE TABLE `Account` (
+    `userId` VARCHAR(191) NOT NULL,
+    `typeasd` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `refresh_token` TEXT NULL,
+    `access_token` TEXT NULL,
+    `expires_at` INTEGER NULL,
+    `token_type` VARCHAR(191) NULL,
+    `scope` VARCHAR(191) NULL,
+    `id_token` TEXT NULL,
+    `session_state` VARCHAR(191) NULL,
+    `id` VARCHAR(191) NOT NULL,
 
---     UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
---     PRIMARY KEY (`id`)
--- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- CreateTable
+CreateTable
 CREATE TABLE `Bug` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
